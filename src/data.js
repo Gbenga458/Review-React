648f7c537e658dd305a99e5400850c77624e4555
unknown <gbengaTec>
2024-04-12 19:31:00 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Ologbonsaiye John',
+    job: 'Content writer',
+    image:
+      'https://scontent.flos1-1.fna.fbcdn.net/v/t39.30808-6/353666846_278087774696696_7706680119603827508_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHFoOkPmnO2fUlxrRXZBAEgdoScp3aZfql2hJyndpl-qdL0SplA3yNnpnRldecoffGJ6W4nqHVV28ZYA9namtBD&_nc_ohc=4wn_Jiq91okAb7a10VF&_nc_zt=23&_nc_ht=scontent.flos1-1.fna&oh=00_AfAKYiUUJfLfusihYDG6t7QVF7iyX3UqkG7yPNhP1WGpvQ&oe=661F2074',
+    text:
+      "I studied History and International Studies, i am an ambivert, i love to watch football, i am a conservationist and like to have fun, i like to help people ",
+  },
+  {
+    id: 2,
+    name: 'Ologbonsaiye Feranmi',
+    job: 'Content writer',
+    image:
+      'https://scontent.flos1-1.fna.fbcdn.net/v/t39.30808-6/285388095_3337088813188743_8651766279465566833_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHIZE167oHZ3GQGm_hjMGd442zo09XWSCrjbOjT1dZIKmqSbei_VOIhGV3BlkMn6ndgsSYAJktAWuPUVz_1cO0q&_nc_ohc=9LXXc1huZ28Ab7bAfI_&_nc_zt=23&_nc_ht=scontent.flos1-1.fna&oh=00_AfAoz0K12MoF8jzwKzVLtBx8Jcot81jEnQ4CrQ6gXhaxBw&oe=661EF2AA',
+    text:
+      'i am an agriculturist, i love to help people and i am hardworking, i am an ambivert and i can chose to be a full time introvert',
+  },
+  {
+    id: 3,
+    name: 'Ologbonsaiye Christianah and Kids',
+    job: 'Educationist',
+    image:
+      'https://scontent.flos1-2.fna.fbcdn.net/v/t39.30808-6/271446764_686918482715050_629237611513825794_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHMJ_FUmxfikerY2g7D9GbimeLJqcWkVR6Z4smpxaRVHvtbfiJObmpytp1ooxU1TP6ddbBQ1s60qQ6vYD6kA5u1&_nc_ohc=hiIOCi8p2mYAb6xr9AY&_nc_zt=23&_nc_ht=scontent.flos1-2.fna&oh=00_AfCI2HbM9NfpwltGEbubvy4gPknOJD5-HnUO_6KCaAUt3w&oe=661EEE02',
+    text:
+      'i am an extrovert, i love to smile and i have passion for teaching , i like to make friends',
+  },
+  {
+    id: 4,
+    name: 'Idowu Abiodun',
+    job: 'Economist',
+    image:
+      'https://scontent.flos1-1.fna.fbcdn.net/v/t1.6435-9/122858099_2910028842616820_301581124643039580_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeH0yyFalBaWZtxwfSjezZy3epaY-2K5kih6lpj7YrmSKFo5rm_atrTc71k1tK9fRa7tWX8PgGmGOj-4g6of2BSI&_nc_ohc=YM4uAkq0QlgAb7ZoZV0&_nc_ht=scontent.flos1-1.fna&oh=00_AfB2ece6DFJe2rE0rLLSCQkNto46bz9pSGhv9jEMJ-mg_A&oe=664091C0',
+    text:
+      'i am an economist, working hard is really a fun thing to me, i am an ambivert ',
+  },
+];
+
+export default reviews;
