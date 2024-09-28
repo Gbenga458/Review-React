821da88b4eef283d3aef7bd997a5d5c0fd73be2e
Gbenga Busayo <154677191+Gbenga458@users.noreply.github.com>
2024-09-28 10:52:59 -0700

    Update data.js
    edited

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ const reviews = [
     name: 'Ologbonsaiye John',
     job: 'Content writer',
     image:
-      'https://scontent.flos1-1.fna.fbcdn.net/v/t39.30808-6/353666846_278087774696696_7706680119603827508_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHFoOkPmnO2fUlxrRXZBAEgdoScp3aZfql2hJyndpl-qdL0SplA3yNnpnRldecoffGJ6W4nqHVV28ZYA9namtBD&_nc_ohc=4wn_Jiq91okAb7a10VF&_nc_zt=23&_nc_ht=scontent.flos1-1.fna&oh=00_AfAKYiUUJfLfusihYDG6t7QVF7iyX3UqkG7yPNhP1WGpvQ&oe=661F2074',
+      'https://scontent.flos5-2.fna.fbcdn.net/v/t39.30808-6/449695301_491509720021166_3824176645003138332_n.jpg?stp=dst-jpg_s960x960&_nc_cat=104&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeFFJ2rfzyNq1uw8ndpCFI06Jrosyfa1L0gmuizJ9rUvSM6grvl8d3nQQm4UGaY-cO4RM4uJB1LgaNeJ4qgLFvxp&_nc_ohc=5vvQygA9a0MQ7kNvgGF3lLu&_nc_zt=23&_nc_ht=scontent.flos5-2.fna&_nc_gid=ABC0qqmazW7yT8cYsfFIp2t&oh=00_AYAIrSSknLXKQJ0L6bPiq7r-p84Hec9pw_ialSW3RhEcKQ&oe=66FE07F4',
     text:
       "I studied History and International Studies, i am an ambivert, i love to watch football, i am a conservationist and like to have fun, i like to help people ",
   },
@@ -13,7 +13,7 @@ const reviews = [
     name: 'Ologbonsaiye Feranmi',
     job: 'Content writer',
     image:
-      'https://scontent.flos1-1.fna.fbcdn.net/v/t39.30808-6/285388095_3337088813188743_8651766279465566833_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHIZE167oHZ3GQGm_hjMGd442zo09XWSCrjbOjT1dZIKmqSbei_VOIhGV3BlkMn6ndgsSYAJktAWuPUVz_1cO0q&_nc_ohc=9LXXc1huZ28Ab7bAfI_&_nc_zt=23&_nc_ht=scontent.flos1-1.fna&oh=00_AfAoz0K12MoF8jzwKzVLtBx8Jcot81jEnQ4CrQ6gXhaxBw&oe=661EF2AA',
+      'https://scontent.flos5-2.fna.fbcdn.net/v/t39.30808-1/256766682_3187452188152407_446948261677286510_n.jpg?stp=c0.0.1511.1511a_dst-jpg_s200x200&_nc_cat=100&ccb=1-7&_nc_sid=0ecb9b&_nc_eui2=AeFOwz0pGQ0RMdndfbdhjMOnUk-5Hde_SBNST7kd179IEzoCL297v_A4HFJSNEyJy00jeygCru_CH3TnOtEC7_KA&_nc_ohc=IOvIiTtvKAcQ7kNvgHFJAAh&_nc_ht=scontent.flos5-2.fna&_nc_gid=AV0jhG_PSuiujMGHy7JGUjm&oh=00_AYAv2XeAanboGfcpmTyLirDL0Xipto81eqiEvw7GS6tt-w&oe=66FE18DB',
     text:
       'i am an agriculturist, i love to help people and i am hardworking, i am an ambivert and i can chose to be a full time introvert',
   },
@@ -22,7 +22,7 @@ const reviews = [
     name: 'Ologbonsaiye Christianah and Kids',
     job: 'Educationist',
     image:
-      'https://scontent.flos1-2.fna.fbcdn.net/v/t39.30808-6/271446764_686918482715050_629237611513825794_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHMJ_FUmxfikerY2g7D9GbimeLJqcWkVR6Z4smpxaRVHvtbfiJObmpytp1ooxU1TP6ddbBQ1s60qQ6vYD6kA5u1&_nc_ohc=hiIOCi8p2mYAb6xr9AY&_nc_zt=23&_nc_ht=scontent.flos1-2.fna&oh=00_AfCI2HbM9NfpwltGEbubvy4gPknOJD5-HnUO_6KCaAUt3w&oe=661EEE02',
+      'https://scontent.flos5-2.fna.fbcdn.net/v/t39.30808-6/271446764_686918482715050_629237611513825794_n.jpg?stp=dst-jpg_s960x960&_nc_cat=100&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeHMJ_FUmxfikerY2g7D9GbimeLJqcWkVR6Z4smpxaRVHvtbfiJObmpytp1ooxU1TP6ddbBQ1s60qQ6vYD6kA5u1&_nc_ohc=oZgZGQ7aN-cQ7kNvgEKb3AY&_nc_zt=23&_nc_ht=scontent.flos5-2.fna&oh=00_AYD_8WmFtk0D338QazQd7QOjCBtZwnIWssqL7m1mf86kFA&oe=66FE2C02',
     text:
       'i am an extrovert, i love to smile and i have passion for teaching , i like to make friends',
   },
@@ -31,7 +31,7 @@ const reviews = [
     name: 'Idowu Abiodun',
     job: 'Economist',
     image:
-      'https://scontent.flos1-1.fna.fbcdn.net/v/t1.6435-9/122858099_2910028842616820_301581124643039580_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeH0yyFalBaWZtxwfSjezZy3epaY-2K5kih6lpj7YrmSKFo5rm_atrTc71k1tK9fRa7tWX8PgGmGOj-4g6of2BSI&_nc_ohc=YM4uAkq0QlgAb7ZoZV0&_nc_ht=scontent.flos1-1.fna&oh=00_AfB2ece6DFJe2rE0rLLSCQkNto46bz9pSGhv9jEMJ-mg_A&oe=664091C0',
+      'https://scontent.flos5-2.fna.fbcdn.net/v/t1.6435-1/122858099_2910028842616820_301581124643039580_n.jpg?stp=c0.81.816.816a_dst-jpg_s200x200&_nc_cat=106&ccb=1-7&_nc_sid=e4545e&_nc_eui2=AeH0yyFalBaWZtxwfSjezZy3epaY-2K5kih6lpj7YrmSKFo5rm_atrTc71k1tK9fRa7tWX8PgGmGOj-4g6of2BSI&_nc_ohc=ZQXiTb2RFmYQ7kNvgENoycy&_nc_ht=scontent.flos5-2.fna&_nc_gid=A52119BhKxINnD3qRb04z4L&oh=00_AYDQeGaB-UhNs5Ke_n46E97D6iRVNsvtWnVOyB4MgvvQTw&oe=671FBB76',
     text:
       'i am an economist, working hard is really a fun thing to me, i am an ambivert ',
   },
